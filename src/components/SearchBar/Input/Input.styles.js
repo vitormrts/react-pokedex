3b3fdd6c7e8bf5912464442ebeca5c2fcad737d9
Vitor Martins Cruz <vitormartinscruz.dev@gmail.com>
2022-06-08 16:49:14 -0300

    Update index.html

--- src/components/SearchBar/Input/Input.styles.js
+++ src/components/SearchBar/Input/Input.styles.js
@@ -6,23 +6,21 @@ export const Form = styled.form`
 `;
 
 export const InputPokemon = styled.input`
-  ${({ theme }) => css`
-    border: 0;
-    box-shadow: 0px 5px 10px 1px rgb(0, 0, 0, 0.1);
-    padding: 16px 24px;
-    border-radius: 16px;
-    display: flex;
-    align-content: center;
-    justify-content: center;
-    font-size: 14px;
-    line-height: 14px;
-    font-weight: 500;
-    width: 100%;
-    min-height: 62px;
-    &:focus {
-      outline: 0;
-    }
-  `}
+  border: 0;
+  box-shadow: 0px 5px 10px 1px rgb(0, 0, 0, 0.1);
+  padding: 16px 24px;
+  border-radius: 16px;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 14px;
+  font-weight: 500;
+  width: 100%;
+  min-height: 62px;
+  &:focus {
+    outline: 0;
+  }
 `;
 
 export const Button = styled.button`
